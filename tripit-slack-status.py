b185d00bdad2372c7cb45d19ef34f61434a85525
David Needham <unrealnighthawk@gmail.com>
2018-04-27 02:14:32 -0500

    Changing the default behavior of the script to fit my needs
     - Moving the environment variables out of the .bash_profile / .zshrc and into this file.
     - Adding a "summary" variable that pulls in my TripIt event summary (event name).
     - If I'm traveling, display a status that shows the event name and travel dates.
     - If I'm not traveling, revert to my default WFH status.

--- tripit-slack-status.py
+++ tripit-slack-status.py
@@ -1,19 +1,22 @@
 import datetime
-import os
 from icalendar import Calendar
 from urllib.request import urlopen, Request
 from urllib.parse import urlencode
 
+# Define the variables
+TRIPIT_ICAL_URL='enter-your-trip-ical-url'
+SLACK_API_TOKEN='enter-your-slack-api-token'
+TRIPIT_HOME='enter the city, state of your home'
+
 # Read the calendar stream.
-ical_url = os.environ.get('TRIPIT_ICAL_URL')
-ical_reader = urlopen(ical_url)
+ical_reader = urlopen(TRIPIT_ICAL_URL)
 ical = ical_reader.read()
 
 # Setup data.
 today = datetime.date.today()
 current_location_start = today
-current_location = os.environ.get('TRIPIT_HOME')
-next_location = os.environ.get('TRIPIT_HOME')
+current_location = TRIPIT_HOME
+next_location = TRIPIT_HOME
 
 # Parse the calendar stream.
 cal = Calendar.from_ical(ical)
@@ -21,6 +24,9 @@ for event in cal.walk('vevent'):
     dtstart = event.get('dtstart').dt
     dtend = event.get('dtend').dt
     location = event.get('location')
+    summary = event.get('summary')
+
+
     # Trips only have dates, not times.
     if isinstance(dtstart, datetime.date) and not isinstance(dtstart, datetime.datetime):
         # If the trip has ended, ignore it.
@@ -30,6 +36,7 @@ for event in cal.walk('vevent'):
         # If we're in the middle of a trip, set it as current.
         if dtstart <= today and today < dtend:
             current_location = location
+            current_summary = summary
             current_location_start = dtstart
             current_location_end = dtend
             next_location_start = dtend
@@ -37,28 +44,33 @@ for event in cal.walk('vevent'):
         # If we're seeing a future event
         if dtstart > today:
             # If coming from home, this is the next trip.
-            if current_location == os.environ.get('TRIPIT_HOME'):
+            if current_location == TRIPIT_HOME:
+                next_summary = summary
                 next_location = location
                 next_location_start = dtstart
             # If coming from another trip that ends after or when this starts, this is next trip.
             elif current_location_end and current_location_end >= dtstart:
+                next_summary = summary
                 next_location = location
                 next_location_start = dtstart
             break;
 
 
 # Pretty format the status info and account for no upcoming trips.
-if current_location != next_location:
-    status = '{c.month}/{c.day}: {cloc} .. {n.month}/{n.day}: {nloc}'.format(c=current_location_start, cloc=current_location, n=next_location_start, nloc=next_location)
+# If I'm traveling, say where I'm at and how long I'll be there for.
+if current_location != TRIPIT_HOME:
+    status = 'Traveling {c.month}/{c.day} -> {n.month}/{n.day} for {summary}'.format(c=current_location_start, cloc=current_location, n=current_location_end, summary=current_summary)
+    emoji = ':airplane:' or ''
+# If I'm not traveling, use my default WFH status.
 else:
-    status = '{c.month}/{c.day}: {cloc}'.format(c=current_location_start, cloc=current_location)
+    status = 'Working from home'
+    emoji = ':house:' or ''
 
-emoji = os.environ.get('SLACK_STATUS_EMOJI') or ''
 
 # Setup the Slack API info.
 slack_url = 'https://slack.com/api/users.profile.set'
 slack_profile = '{"status_text":"' + status + '", "status_emoji": "' + emoji + '"}'
-slack_token = os.environ.get('SLACK_API_TOKEN')
+slack_token = SLACK_API_TOKEN
 
 # Post the status to Slack.
 post_data = {'token': slack_token, 'profile': slack_profile}
