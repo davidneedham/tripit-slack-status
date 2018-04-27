359d2fe5a893e6fb2696521afdd903d4cc232568
David Needham <unrealnighthawk@gmail.com>
2018-04-27 03:19:30 -0500

    Adding in support for updating multiple slack teams at once

--- tripit-slack-status.py
+++ tripit-slack-status.py
@@ -4,9 +4,16 @@ from urllib.request import urlopen, Request
 from urllib.parse import urlencode
 
 # Define the variables
-TRIPIT_ICAL_URL='enter-your-trip-ical-url'
-SLACK_API_TOKEN='enter-your-slack-api-token'
-TRIPIT_HOME='enter the city, state of your home'
+TRIPIT_ICAL_URL = 'YOUR TRIPIT ICAL'
+TRIPIT_HOME = 'YOUR HOME LOCATION'
+SLACK_API_TOKEN = [[]]
+SLACK_API_TOKEN[0].extend(['SLACK NAME1','YOUR FIRST SLACK API TOKEN'])
+SLACK_API_TOKEN.append(['SLACK NAME2','YOUR SECOND SLACK API TOKEN'])
+SLACK_API_TOKEN.append(['SLACK NAME3','YOUR THIRD SLACK API TOKEN'])
+SLACK_API_TOKEN.append(['SLACK NAME4','YOUR FOURTH SLACK API TOKEN'])
+SLACK_API_TOKEN.append(['SLACK NAME5','YOUR FIFTH SLACK API TOKEN'])
+SLACK_API_TOKEN.append(['SLACK NAME6','YOUR SIXTH SLACK API TOKEN'])
+SLACK_API_TOKEN.append(['SLACK NAME7','YOUR SEVENTH SLACK API TOKEN'])
 
 # Read the calendar stream.
 ical_reader = urlopen(TRIPIT_ICAL_URL)
@@ -57,11 +64,9 @@ for event in cal.walk('vevent'):
 
 
 # Pretty format the status info and account for no upcoming trips.
-# If I'm traveling, say where I'm at and how long I'll be there for.
 if current_location != TRIPIT_HOME:
     status = 'Traveling {c.month}/{c.day} -> {n.month}/{n.day} for {summary}'.format(c=current_location_start, cloc=current_location, n=current_location_end, summary=current_summary)
     emoji = ':airplane:' or ''
-# If I'm not traveling, use my default WFH status.
 else:
     status = 'Working from home'
     emoji = ':house:' or ''
@@ -72,10 +77,13 @@ slack_url = 'https://slack.com/api/users.profile.set'
 slack_profile = '{"status_text":"' + status + '", "status_emoji": "' + emoji + '"}'
 
 # Post the status to Slack.
-post_data = {'token': SLACK_API_TOKEN, 'profile': slack_profile}
-req = Request(slack_url, urlencode(post_data).encode())
-resp = urlopen(req)
-if resp.status == 200:
-    print('Status updated: ' + status)
-else:
-    print(resp.read())
+#for slack_token in SLACK_API_TOKEN:
+
+for i in range(len(SLACK_API_TOKEN)):
+    post_data = {'token': SLACK_API_TOKEN[i][1], 'profile': slack_profile}
+    req = Request(slack_url, urlencode(post_data).encode())
+    resp = urlopen(req)
+    if resp.status == 200:
+        print(SLACK_API_TOKEN[i][0] + ' Slack status updated: ' + status)
+    else:
+        print(resp.read())
